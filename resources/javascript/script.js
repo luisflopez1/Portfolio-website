06d14bfdb3200d9a5756575f47f8e0df481a9a32
luis lopez <Luis.lopez0417@gmail.com>
2021-06-30 19:28:22 -0400

    styled website
    added backgrounds
    adjusted size of images
    added profile image plus animations and transitions

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,17 +1,26 @@
 
 let text = document.getElementById('main-text')
 let bio = document.getElementById('bio')
+let profile = document.getElementById('profile')
 
 
 const myScrollFunc = () => {
   var y = window.scrollY;
-  if (y <= 800) {
+  if (y <= 1200) {
     text.style.display = "block"
     bio.style.display = "none"
+    profile.style.display = "none"
   } else {
     text.style.display = "none"
     bio.style.display = "block"
+    
   }
+};
+
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+      profile.style.display = "block";
+    }
 };
 
 window.addEventListener("scroll", myScrollFunc);

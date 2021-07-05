471cc958ea864ad19f36f23b1053f31c1550eaa8
luis lopez <Luis.lopez0417@gmail.com>
2021-07-05 15:31:10 -0400

    added folders
    wrapped work elements in flex
    rotated nav,
    added typing animation

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -2,6 +2,26 @@
 let text = document.getElementById('main-text')
 let bio = document.getElementById('bio')
 let profile = document.getElementById('profile')
+let screen = document.getElementById('screen')
+let paintings = document.getElementById('paintings')
+let editing = document.getElementById('editing')
+let brand = document.getElementById('brand')
+let info = document.getElementById('info')
+
+
+var i = 0;
+var txt = 'You need an Artist?\nGraphic Designer?\nWeb Developer?';
+var speed = 100;
+
+document.onload = typeWriter();
+
+function typeWriter() {
+  if (i < txt.length) {
+    text.innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
 
 
 const myScrollFunc = () => {
@@ -16,6 +36,64 @@ const myScrollFunc = () => {
     
   }
 };
+
+//paintings folder
+
+paintings.onmouseover = function(){
+  screen.style.backgroundImage = " url(./resources/images/test.gif)";
+  screen.style.opacity = "1";
+  screen.style.backgroundSize = "contain";
+  screen.style.backgroundColor = "rgba(255, 255, 255, .10)";
+  screen.style.backgroundPosition = "center";
+  screen.style.backdropFilter = "blur(8px)"
+  info.innerHTML = "File found: Portrait Paintings"
+};
+
+paintings.onmouseout = function(){
+  screen.style.backgroundImage = "";
+  screen.style.opacity = "";
+  screen.style.backgroundSize = "";
+  info.innerHTML = "Locating  file . . . "
+};
+
+//editing folder
+
+editing.onmouseover = function(){
+  screen.style.backgroundImage = " url(./resources/images/photoediting.gif)";
+  screen.style.opacity = "1";
+  screen.style.backgroundSize = "contain";
+  screen.style.backgroundColor = "rgba(255, 255, 255, .10)";
+  screen.style.backgroundPosition = "center";
+  screen.style.backdropFilter = "blur(8px)"
+  info.innerHTML = "File found: Photo Editing"
+};
+
+editing.onmouseout = function(){
+  screen.style.backgroundImage = "";
+  screen.style.opacity = "";
+  screen.style.backgroundSize = "";
+  info.innerHTML = "Locating  file . . . "
+};
+
+//brand identity folder
+
+brand.onmouseover = function(){
+  screen.style.backgroundImage = " url(./resources/images/brandidentity.gif)";
+  screen.style.opacity = "1";
+  screen.style.backgroundSize = "contain";
+  screen.style.backgroundColor = "rgba(255, 255, 255, .10)";
+  screen.style.backgroundPosition = "center";
+  screen.style.backdropFilter = "blur(8px)"
+  info.innerHTML = "File found: Brand Identity"
+};
+
+brand.onmouseout = function(){
+  screen.style.backgroundImage = "";
+  screen.style.opacity = "";
+  screen.style.backgroundSize = "";
+  info.innerHTML = "Locating  file . . . "
+};
+
 
 window.onscroll = function(ev) {
     if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
